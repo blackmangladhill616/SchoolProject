1d5e04296e5de198337cf8fd4b14e14a75853b41
blackmangladhill616 <blackmangladhill616@gmail.com>
2025-03-07 09:08:29 +0000

    Add 3.py

---
+++ src/3.py
@@ -0,0 +1,9 @@
+
+import random
+
+def get_random_string(length):
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    result = ""
+    for i in range(length):
+        result += random.choice(letters)
+    return result
