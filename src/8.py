b3bd113d8fff09d2c2719d4be4fdaab50daf0998
blackmangladhill616 <blackmangladhill616@gmail.com>
2025-03-12 07:06:39 +0000

    Add 8.py

---
+++ src/8.py
@@ -0,0 +1,11 @@
+def generate_random_code(length=10):
+    import random
+    
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    
+    code = ''
+    for i in range(length):
+        code += random.choice(letters) + random.choice(numbers)
+        
+    return code
