dd7df60ac272446d2181d27e5f1a29bc53157feb
blackmangladhill616 <blackmangladhill616@gmail.com>
2025-04-03 12:37:56 +0000

    Add 24.py

---
+++ src/24.py
@@ -0,0 +1,17 @@
+def add_numbers(a, b):
+    result = a + b
+    return result
+
+def subtract(a, b):
+    difference = a - b
+    return difference
+
+def multiply(numbers):
+    product = 1
+    for number in numbers:
+        product *= number
+    return product
+
+def divide(number, divisor):
+    quotient = number / divisor
+    return quotient
